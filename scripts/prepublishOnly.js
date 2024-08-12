0fbf9cab9b1c806b780108d44e993d4170892962
zhuddan <yobaci@163.com>
2024-08-12 14:20:59 +0800

    fix

--- scripts/prepublishOnly.js
+++ scripts/prepublishOnly.js
@@ -1,4 +1,4 @@
-// const fs = require('node:fs')
+const fs = require('node:fs')
 const path = require('node:path')
 
 const packageJsonPath = path.resolve(__dirname, '../package.json')
@@ -7,4 +7,4 @@ const packageJson = require(packageJsonPath)
 packageJson.devDependencies = {}
 packageJson.peerDependencies = {}
 
-// fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
