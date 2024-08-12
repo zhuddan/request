e63ed4e938e5fa140e504ba4e3e60dfbf4451280
zhuddan <yobaci@163.com>
2024-08-12 12:04:22 +0800

    fix

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -4,8 +4,6 @@ const path = require('node:path')
 const packageJsonPath = path.resolve(__dirname, '../package.json')
 const packageJson = require(packageJsonPath)
 
-// 动态修改 package.json 的内容
-packageJson.scripts.devDependencies = {}
+packageJson.devDependencies = {}
 
-// 将修改后的内容写回 package.json
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
